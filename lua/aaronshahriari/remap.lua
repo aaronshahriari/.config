6997b52f3b4e25f9e1d48af01cd409fb6e0dd0c2
aaronshahriari <aaronshahriari@gmail.com>
2024-01-12 11:11:00 -0500

    updated remap with new tab bind

--- lua/aaronshahriari/remap.lua
+++ lua/aaronshahriari/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.g.netrw_bufsettings = 'noma nomod nu nowrap ro nobl'
 vim.cmd('autocmd FileType netrw set nu')
 
+-- create new tab
+vim.api.nvim_set_keymap('n', '<leader>T', ':tabnew<CR>', { noremap = true, silent = true })
 -- Map <leader>-h and <leader>-l to cycle through tabs
 vim.api.nvim_set_keymap('n', '<leader>h', ':tabprevious<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>l', ':tabnext<CR>', { noremap = true, silent = true })
