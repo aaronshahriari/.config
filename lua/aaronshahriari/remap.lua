7733267dc2217494e8b41d9606923ba14c58bdef
Aaron Shahriari <aaronshahriari@gmail.com>
2024-01-18 16:44:11 -0500

    updated moving across splits because interfere with harpoon

--- lua/aaronshahriari/remap.lua
+++ lua/aaronshahriari/remap.lua
@@ -5,10 +5,10 @@ vim.g.netrw_bufsettings = "noma nomod nu nowrap ro nobl"
 vim.cmd("autocmd FileType netrw set nu")
 
 -- Map these to move through splits
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
 
 -- Map to change split size
 vim.keymap.set("n", "<Left>", "<C-w>3<")
