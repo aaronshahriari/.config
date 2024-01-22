085a36c5375980d59ce09c06a065fe3a8b60a455
Aaron Shahriari <aaronshahriari@gmail.com>
2024-01-22 14:34:41 -0500

    when terminal is open it doesnt have the line number set to 8 or any relative at all

--- lua/aaronshahriari/set.lua
+++ lua/aaronshahriari/set.lua
@@ -39,6 +39,14 @@ vim.opt.conceallevel = 1
 -- change in builtin.lua found here -> ~\Local\nvim-data\site\pack\packer\start\midnight\lua\midnight\highlight
 -- vim.cmd('highlight NonText guifg=#575757')
 
+-- set the window options for :term
+vim.api.nvim_exec([[
+  augroup TermWindowConfig
+    autocmd!
+    autocmd TermOpen * setlocal nonumber norelativenumber signcolumn=no
+  augroup END
+]], false)
+
 -- automatically enter insert mode when switching to :term window
 vim.api.nvim_exec([[
   augroup AutoEnterInsert
