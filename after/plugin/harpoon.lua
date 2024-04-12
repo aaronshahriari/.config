13eee6b3d1d51b67194ff0283aa96966c3c0c78f
Aaron Shahriari <aaronshahriari@gmail.com>
2024-04-12 12:20:42 -0400

    brundage laptop changes to updates

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -21,3 +21,6 @@ vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- lua
+vim.cmd [[ autocmd Filetype harpoon setlocal cursorline ]]
