86da456797b14fa856f8a473e1743259decbe077
Aaron Shahriari <aaronshahriari@gmail.com>
2024-04-15 08:51:47 -0400

    updated obsidian to have some characters for todo boxes; change harpoon append -> add ** updated to harpoon 2 on my machine **

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -2,12 +2,27 @@ require("obsidian").setup({
     workspaces = {
         {
             name = "Personal",
-            path = "~/../../mnt/c/Users/aaron/Onedrive/Documents/Aaron_Vault/Personal",
+            path = "~/../../mnt/c/Users/AaronShahriari/Aaron_Vault/Personal",
         },
         {
             name = "BrundageGroup",
-            path = "~/../../mnt/c/Users/aaron/Onedrive/Documents/Aaron_Vault/BrundageGroup",
+            path = "~/../../mnt/c/Users/AaronShahriari/Aaron_Vault/BrundageGroup",
         },
     },
-    -- see below for full list of options 👇
+    ui = {
+        enable = true,  -- set to false to disable all additional syntax features
+        update_debounce = 200,  -- update delay after a text change (in milliseconds)
+        -- Define how various check-boxes are displayed
+        checkboxes = {
+            [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+            ["x"] = { char = "✓", hl_group = "ObsidianDone" },
+            [">"] = { char = "»", hl_group = "ObsidianRightArrow" },
+            ["~"] = { char = "X", hl_group = "ObsidianTilde" },
+            -- Replace the above with this if you don't have a patched font:
+            -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+            -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+            -- You can also add more custom ones...
+        },    -- see below for full list of options 👇
+    }
 })
