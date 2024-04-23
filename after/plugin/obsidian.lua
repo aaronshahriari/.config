b3bf9f646dd92c1601b2bf2f80959ff4c6f42cea
Aaron Shahriari <aaronshahriari@gmail.com>
2024-04-23 16:11:22 -0400

    updated obsidian for gf command

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -30,3 +30,10 @@ require("obsidian").setup({
         vim.keymap.set("n", "<C-o>", "<cmd>ObsidianOpen<CR>")
     }
 })
+vim.keymap.set("n", "gf", function()
+  if require("obsidian").util.cursor_on_markdown_link() then
+    return "<cmd>ObsidianFollowLink<CR>"
+  else
+    return "gf"
+  end
+end, { noremap = false, expr = true })
