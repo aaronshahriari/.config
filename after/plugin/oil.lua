b34753c9c0276b32392bbd2b2d8a84c4b6eadc0f
Aaron Shahriari <aaronshahriari@gmail.com>
2024-05-24 04:28:20 -0400

    update: oil float size

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -8,7 +8,7 @@ require("oil").setup({
         -- Padding around the floating window
         padding = 2,
         max_width = 70,
-        max_height = 50,
+        max_height = 30,
         border = "single",
         win_options = {
             winblend = 0,
