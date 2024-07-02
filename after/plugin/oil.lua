063dd4023048c706cd67dc5e287728f4ae0fd045
Aaron Shahriari <aaronshahriari@gmail.com>
2024-07-02 09:27:38 -0400

    update added oil config stuff

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -1,22 +1,33 @@
 require("oil").setup({
-    default_file_explorer = false,
-    columns = { "icon" },
+    default_file_explorer = true,
+    columns = {
+        "icon",
+    },
     view_options = {
         show_hidden = true,
     },
-    float = {
-        -- Padding around the floating window
-        padding = 2,
-        max_width = 70,
-        max_height = 30,
-        border = "single",
-        win_options = {
-            winblend = 0,
-        },
+    keymaps = {
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-x>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-l>"] = "actions.refresh",
     },
+    -- float = {
+    --     -- Padding around the floating window
+    --     padding = 2,
+    --     max_width = 70,
+    --     max_height = 30,
+    --     border = "single",
+    --     win_options = {
+    --         winblend = 0,
+    --     },
+    -- },
       skip_confirm_for_simple_edits = true,
       prompt_save_on_select_new_entry = false,
 })
 
 -- Open parent directory in floating window
-vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+-- vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+vim.keymap.set("n", "<leader>pv", require("oil").open)
